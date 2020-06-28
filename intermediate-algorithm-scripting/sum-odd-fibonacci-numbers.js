a68213cc5d7c47ede12b42990487f33009418780
Wadson Fourrien <5272265+wadsonfourrien@users.noreply.github.com>
2020-06-28 09:22:43 -0400

    Update sum-odd-fibonacci-numbers.js

--- intermediate-algorithm-scripting/sum-odd-fibonacci-numbers.js
+++ intermediate-algorithm-scripting/sum-odd-fibonacci-numbers.js
@@ -22,3 +22,23 @@ function sumFibs(num)
 }
 
 sumFibs(4);
+
+
+
+// CODE REFACTORED ~ 2020
+
+function sumFibs(num) {
+  // store the two last value and the sum
+  let bLast = 1, last = 1, sum = 1;
+  while(last <= num) {
+    if (last % 2 == 1) {
+      sum += last;
+    }
+    last += bLast; // last become last + before-last
+    bLast = last - bLast; // before-last becomes what was once last
+  }
+  return sum;
+}
+sumFibs(4);
+
+
